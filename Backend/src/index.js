cb02a6e0bbfb69e8d37aaef1eb8a5be20dc0c16a
TrungAnh <trunganh.edu.net@gmail.com>
2023-06-30 12:09:15 +0700

    test connect database

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,10 +1,30 @@
+const cors = require('cors');
 const express = require('express')
-const app = express();
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const app = express()
+const connectDB = require('./config/connect')
+const db = require('./models/index')
 
-// index page
-app.get('/', function(req, res) {
-  res.send('pages/index');
-});
+
+app.use(cors())
+// HTTP logger  
+app.use(morgan('combined'))
+// template engine
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+connectDB()
+
+app.get('/' , async (req, res) => {
+  try {
+    let data = await db.User.findAll()
+    console.log(data)
+      res.send("Trung Anh")
+  } catch (error) {
+      console.log(error)
+  }
+})
 
 app.listen(8080);
 console.log('Server is listening on port 8080');
